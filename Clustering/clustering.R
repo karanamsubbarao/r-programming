1a5fa49ec1e3c6cb877421d85added97e592c5fa
karanamsubbarao <karanamsubbarao@gmail.com>
2016-01-07 16:22:31 +0530

    clustering

--- Clustering/clustering.R
+++ Clustering/clustering.R
@@ -1,6 +1,6 @@
 ## K-means Clustering 
 
-data <- read.csv("~/Data Science Mini XTR/Clustering/data.csv", header=TRUE, row.names=1)
+data <- read.csv("~/data.csv", header=TRUE, row.names=1)
 km <- kmeans(data, 3, 15)
 
 # print components of km
